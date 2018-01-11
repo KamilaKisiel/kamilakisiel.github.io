5495e17c87e6fddfb50980097b74dabdda3c7d7d
KamilaKisiel <kisiel.kamila@gmail.com>
2018-01-11 21:19:02 +0100

    jQuery task, second approach

--- zadanie 8.js
+++ zadanie 8.js
@@ -1,17 +1,31 @@
-var $container = $("<div/>").addClass("container");
-var $rowOne = $("<div/>".addClass("row");
-var $rowTwo = $("<div/>".addClass("row");
-var $rowThree = $("<div/>".addClass("row");
+const $container = $('<div class="container">');
+// var $rowOne = $('<div>').addClass("row");
+// var $rowTwo = $('<div>').addClass("row");
+// var $rowThree = $('<div>').addClass("row");
+
+const $makeParagraph = () => $('<p class="desription">');
+const $makeRow = () => $('<div class="row">');
+
+const $rowOne = $makeRow()
+    .attr('id', 'aaa').append($makeParagraph('jeden'))
+    .attr('id', 'bbb').append($makeParagraph('dwa'))
+    .attr('id', 'ccc').append($makeParagraph('trzy'));
+
+const $rowTwo = $makeRow()
+    .css('color', 'red').append($makeParagraph('cztery'))
+    .css('color', 'red').append($makeParagraph('pięć'));
+
+const $rowThree = $makeRow()
+    .attr('data-product', '300').append($makeParagraph('sześć'))
+    .attr('data-product', '400').append($makeParagraph('siedem'))
+    .attr('data-product', '500').append($makeParagraph('osiem'));
 
 
-var $myContainer =
-    $container.append(
-        $rowOne.append("<div/>", "<div/>", "<div/>"))
-        .append(
-        $rowTwo.append("<div/>", "<div/>"))
-        .append(
-        $rowThree.append("<div/>", "<div/>"));
+$container
+        .append($rowOne)
+        .append($rowTwo)
+        .append($rowThree);
 
 
-$myContainer.appendTo("body");
+$container.appendTo("body");
 
