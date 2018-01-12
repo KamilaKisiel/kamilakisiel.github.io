d748f454081deb609ac7879fc37b3404c828e849
KamilaKisiel <kisiel.kamila@gmail.com>
2018-01-12 20:35:16 +0100

    ready to review @pukie

--- zadanie 8.js
+++ zadanie 8.js
@@ -1,24 +1,21 @@
-const $container = $('<div class="container">');
-// var $rowOne = $('<div>').addClass("row");
-// var $rowTwo = $('<div>').addClass("row");
-// var $rowThree = $('<div>').addClass("row");
-
-const $makeParagraph = () => $('<p class="desription">');
-const $makeRow = () => $('<div class="row">');
+const $container = $('<div>').addClass("container");
+const $makeColumn = (size, number) => $('<div>').addClass('col-' + size + '-' + number);
+const $makeParagraph = () => $('<p>').addClass("desription");
+const $makeRow = () => $('<div>').addClass("row");
 
 const $rowOne = $makeRow()
-    .attr('id', 'aaa').append($makeParagraph('jeden'))
-    .attr('id', 'bbb').append($makeParagraph('dwa'))
-    .attr('id', 'ccc').append($makeParagraph('trzy'));
+    .append($makeColumn('sm', 4).attr('id', 'aaa').append($makeParagraph('jeden'))
+    .append($makeColumn('sm', 4).attr('id', 'bbb').append($makeParagraph('dwa'))
+    .append($makeColumn('sm', 4).attr('id', 'ccc').append($makeParagraph('trzy'));
 
 const $rowTwo = $makeRow()
-    .css('color', 'red').append($makeParagraph('cztery'))
-    .css('color', 'red').append($makeParagraph('pięć'));
+    .append($makeColumn('xs', 6).css('color', 'red').append($makeParagraph('cztery'))
+    .append($makeColumn('xs', 6).css('color', 'red').append($makeParagraph('pięć'));
 
 const $rowThree = $makeRow()
-    .attr('data-product', '300').append($makeParagraph('sześć'))
-    .attr('data-product', '400').append($makeParagraph('siedem'))
-    .attr('data-product', '500').append($makeParagraph('osiem'));
+    .append($makeColumn('md', 3).attr('data-product', '300').append($makeParagraph('sześć'))
+    .append($makeColumn('md', 3).attr('data-product', '400').append($makeParagraph('siedem'))
+    .append($makeColumn('md', 3).attr('data-product', '500').append($makeParagraph('osiem'));
 
 
 $container
@@ -29,3 +26,24 @@ $container
 
 $container.appendTo("body");
 
+/*Za pomoca wylacznie biblioteki jQuery, utwórz nastapujacy kod html:
+    http://jsfiddle.net/h6gp9jnt/
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4" id="aaa"><p class="description">Jeden</p></div>
+        <div class="col-sm-4" id="ccc"><p class="description">Dwa</p></div>
+        <div class="col-sm-4" id="ooo"><p class="description">Trzy</p></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-6" style="color: red;"><p class="description">Cztery</p></div>
+        <div class="col-xs-6" style="color: red;"><p class="description">Piec</p></div>
+    </div>
+    <div class="row">
+        <div class="col-md-3" data-product="300"><p class="description">Szesc</p></div>
+        <div class="col-md-3" data-product="400"><p class="description">Siedem</p></div>
+        <div class="col-md-3" data-product="500"><p class="description">Osiem</p></div>
+    </div>
+</div>
+Przydatne funkcje: addClass, text, append...
+    Powodzenia!
+    */
